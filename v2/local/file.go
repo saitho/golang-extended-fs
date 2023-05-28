46d1ddfe887739815a1d8888ffd0bf3928fb8ed6
Mario Lubenka <me@saitho.me>
2023-05-28 22:26:13 +0200

    fix(HasFile): deal with file not found error

--- v2/local/file.go
+++ v2/local/file.go
@@ -48,5 +48,8 @@ func (p ProtocolHandler) DeleteFile(filePath string) error {
 func (p ProtocolHandler) HasFile(filePath string) (bool, error) {
 	localPath := p.ResolveFilePath(filePath)
 	info, err := os.Stat(localPath)
+	if err.Error() == "file does not exist" {
+		return false, nil
+	}
 	return info != nil, err
 }
